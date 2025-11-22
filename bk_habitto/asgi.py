9c4d73b43e2ca698cb570e67b1699b7133496e13
Max <develop.fos@gmail.com>
2025-11-22 11:14:14 -0400

    refactor(asgi): add http protocol handler and simplify websocket setup
    Add django_asgi_app for http protocol handling and remove redundant parentheses in websocket configuration

--- bk_habitto/asgi.py
+++ bk_habitto/asgi.py
@@ -2,16 +2,18 @@ import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bk_habitto.settings')
 django.setup()
+
+django_asgi_app = get_asgi_application()
 
 from message.routing import websocket_urlpatterns
 from message.middleware import JWTAuthMiddlewareStack, SessionAuthMiddleware
 
 # Usar JWT auth middleware para mejor seguridad
 application = ProtocolTypeRouter({
-    'websocket': JWTAuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
-    ),
+    'http': django_asgi_app,
+    'websocket': JWTAuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
 })
